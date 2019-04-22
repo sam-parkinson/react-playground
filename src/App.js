7de2a8ee2ab22b85cde5175c7e5af1bd4c88f9c0
Sam Parkinson <parkinsonsp42@gmail.com>
2019-04-22 17:59:34 -0400

    update w/ state drills

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Messages from './Messages';
+import TheDate from './state/TheDate';
+import Counter from './state/Counter';
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <h1>YOUR APPLICATION NAME!</h1>
       <Messages name="Messages" unread={0} />
       <Messages name="Notifications" unread={10} />
+      <TheDate />
+      <Counter count={123} />
     </main>
   );
 }
