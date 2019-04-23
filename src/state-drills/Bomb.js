3732fe086e5633436c1508a4491f86afebc46872
Sam Parkinson <parkinsonsp42@gmail.com>
2019-04-23 12:10:22 -0400

    added bomb drill

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
 class Bomb extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0, timer: 'tick' };
+  }
 
+  tickTock = () => {
+    if (this.state.count >= 8) {
+      this.setState({ timer: 'BOOM!' });
+      clearInterval(this.interval);
+    } else if (this.state.count % 2 === 0) {
+      this.setState({ timer: 'tick' });
+    } else {
+      this.setState({ timer: 'tock' });
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ 
+        count: this.state.count + 1 
+      })
+      this.tickTock()
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     return (
-      <div>BOOM!</div>
+      <div>
+        <p>{this.state.timer}</p>
+      </div>
     )
   }
 }
